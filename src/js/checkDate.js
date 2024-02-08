487f11e6aaa1ec3d29945b0aa83b72823b04e0cd
henrique1232H <henriqueau111@gmail.com>
2024-02-08 20:30:03 -0300

    add date

---
+++ src/js/checkDate.js
@@ -0,0 +1,36 @@
+
+export default function checkDate(day, entries) {
+
+    console.log(day)
+
+    if(day === 0) {
+        entries.textContent = "Terça"
+    }
+    
+    if(day === 1){
+        entries.textContent = "Quarta"
+    }
+
+    if(day === 2){
+        entries.textContent = "Quinta"
+    }
+
+    if(day === 3) {
+        entries.textContent = "Sexta"
+    }
+
+    if(day === 4) {
+        entries.textContent = "Sábado"
+    }
+
+    if(day === 5) {
+        entries.textContent = "Domingo"
+    }
+
+    if(day === 6) {
+        entries.textContent = "Segunda"
+    }
+
+
+
+}
--- src/js/showInformation.js
+++ src/js/showInformation.js
@@ -1,28 +1,47 @@
 import AcessIp from "./API/AcessIp.js";
 import meteoblue from "./API/meteoblue.js";
+import checkDate from "./checkDate.js";
 
 export default async function showInformation() {
     const responseMeteoBlue = await meteoblue();
     const {city, region} = await AcessIp();
-    const date = new Date();
-
+    const FunctionDate = new Date();
     const heat = document.querySelectorAll(".heat");
+    const moreDay = document.querySelectorAll(".moreDays h4");
     let value = 0;
 
-    console.log(responseMeteoBlue)
+    console.log(responseMeteoBlue);
+
     
     const weather = {
         climateMean: responseMeteoBlue.data_day.temperature_mean,
         climateMax: responseMeteoBlue.data_day.temperature_max,
         climateMin: responseMeteoBlue.data_day.temperature_min
     }
-
+    
     heat.forEach(entries => {
-      entries.textContent = `${Math.floor(weather.climateMean[value])}ºC`
-      value++
-
+        entries.textContent = `${Math.round(weather.climateMean[value])}ºC`
+        value++
+        
     })
 
+    const time = responseMeteoBlue.data_day.time;
+    
+    value = 0
+
+    moreDay.forEach(entries => {
+
+        const dayEntries = new Date(time[value]);;
+        const day = dayEntries.getDay();
+
+        checkDate(day, entries);
+        value++
+    })
+    
+    
+    const minMax = document.querySelectorAll(".minMax h4");
+    minMax[0].textContent = `${weather.climateMin[0]}ºC`;
+    minMax[1].textContent = `${weather.climateMax[0]}ºC`;
 
 
     const newWeather = weather.climateMean.filter(entries => entries !== weather.climateMean[0]);
@@ -34,6 +53,7 @@ export default async function showInformation() {
         document.documentElement.classList.remove("sun");
         document.documentElement.classList.remove("rain");
         document.documentElement.classList.add("cloud");
+        document.documentElement.classList.remove("night");
     }
 
 
@@ -41,6 +61,14 @@ export default async function showInformation() {
         document.documentElement.classList.add("rain")
         document.documentElement.classList.remove("cloud");
         document.documentElement.classList.remove("sun");
+        document.documentElement.classList.remove("night");
+    }
+
+    if(FunctionDate.getHours() >= 19 || date.getDate <= 5) {
+        document.documentElement.classList.remove("rain")
+        document.documentElement.classList.remove("cloud");
+        document.documentElement.classList.remove("sun");
+        document.documentElement.classList.add("night");
     }
 
     const windspeed = responseMeteoBlue.data_day.windspeed_mean;
